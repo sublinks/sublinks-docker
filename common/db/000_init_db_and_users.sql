20c4b0d83078b0eadde563d4fb011806976a136e
Joe Constant <joe@joeconstant.com>
2024-04-24 00:09:54 -0600

    Switch to use schemas instead of dbs

--- common/db/000_init_db_and_users.sql
+++ common/db/000_init_db_and_users.sql
@@ -1,10 +1,14 @@
 -- create databases
-CREATE DATABASE backend;
-CREATE DATABASE federation;
+CREATE SCHEMA backend;
+CREATE SCHEMA federation;
 
 -- create users and grant rights in postgres
 CREATE ROLE backend WITH LOGIN PASSWORD 'backend';
 CREATE ROLE federation WITH LOGIN PASSWORD 'federation';
-GRANT ALL PRIVILEGES ON DATABASE backend TO backend;
-GRANT ALL PRIVILEGES ON DATABASE federation TO federation;
+GRANT ALL ON SCHEMA backend TO backend;
+GRANT ALL ON SCHEMA federation TO federation;
 
+
+-- set default schema for users
+ALTER USER backend SET search_path TO backend;
+ALTER USER federation SET search_path TO federation;
